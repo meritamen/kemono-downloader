5c6b04cf8cbd136b3866e377241ac08af7f6cac8
Mrjt Jmn <meritamen@sdf.org>
2025-05-21 21:05:20 +0800

    Add a new CLI option specifies whether to use sequential filenames

--- app/Main.hs
+++ app/Main.hs
@@ -6,28 +6,29 @@ import ConvertUrl
 import Downloader
 import FetchJson
 
-data Option = PostUrl String
-
-postUrlP :: Parser Option
-postUrlP = PostUrl <$> argument str
-  (metavar "<URL>" <> help "Specify the Kemono post link to download.")
+data Option = Option String Bool
 
 optionP :: Parser Option
-optionP = postUrlP
+optionP = Option
+  <$> argument str (metavar "<URL>" <> help "Specify the Kemono post link to download.")
+  <*> flag False True
+        ( long "sequential_file"
+       <> short 's'
+       <> help "Use sequential numbering for downloaded file names." )
 
 getOption :: IO Option
 getOption = execParser $ info (optionP <**> helper) $
   fullDesc <> progDesc "Kemono Downloader, Mrjt Jmn<meritamen@sdf.org>"
 
 runOption :: Option -> IO ()
-runOption (PostUrl url) = do
+runOption (Option url isSeq) = do
   case convertPageUrlToApi url of
     Nothing -> putStrLn "Invalid Kemono post URL format."
     Just url -> do
       json <- extractJson url
       case json of
         Nothing -> putStrLn "Invalid json format."
-        Just result -> download result
+        Just result -> download result isSeq
 
 main :: IO ()
 main = getOption >>= runOption
--- src/Downloader.hs
+++ src/Downloader.hs
@@ -11,20 +11,23 @@ import Types
 downloadFile :: FilePath -> String -> String -> IO ()
 downloadFile dir url filename = do
   let outPath = dir </> filename
-  putStrLn $ "Downloading: " ++ filename
+  putStrLn $ "Downloading: " <> filename
   response <- httpLBS =<< parseRequest url
   BS.writeFile outPath (getResponseBody response)
 
-download :: APIResponse -> IO ()
-download apiResponse = do
+download :: APIResponse -> Bool -> IO ()
+download apiResponse isSequentialFileName = do
   let
     targetDir = title . post $ apiResponse
     previewFiles = previews apiResponse
     attachmentFiles = attachments apiResponse
-    allFiles = previewFiles <> attachmentFiles
+    allFiles = zip [1..] $ previewFiles <> attachmentFiles
   createDirectoryIfMissing True targetDir
-  mapConcurrently_ (\f -> downloadFile
-                            "."
-                            (server f </> "data" <> path f)
-                            (targetDir </> name f)) allFiles
+  mapConcurrently_ (\(i, f) -> downloadFile
+                               "."
+                               (server f </> "data" <> path f)
+                               (targetDir </> if isSequentialFileName
+                                              then show i <> takeExtension (name f)
+                                              else name  f))
+                   allFiles
   putStrLn "All downloads completed."
